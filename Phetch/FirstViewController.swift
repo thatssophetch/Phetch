4d45b66c288550a5755d3bc0e3f23af2e5a20669
y2bd <jalo@ucsd.edu>
2015-11-18 07:58:24 -0800

    Initial Commit

---
+++ Phetch/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Phetch
+//
+//  Created by y2bd on 11/18/15.
+//  Copyright (c) 2015 phetch. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
